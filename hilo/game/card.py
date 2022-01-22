67e790948f5b1e281682c8b2166d9c3f8f98bd53
agueroc <96946821+agueroc@users.noreply.github.com>
2022-01-22 15:46:11 -0300

    Update card.py

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,6 +1,24 @@
-class Card:
-   def __init__(self):
-      self.value = 1
+import random
 
-   def deal(self):
-      self.value = 9
+class Card:
+    """A generation of a card 1 to 13.
+       The responsibility of Card is to keep track of the card selected 
+   
+        Attributes:
+        value (int): The number of spots on the side facing up.
+    """
+
+    def __init__(self):
+        """Constructs a new instance of Card.
+        Args:
+            self (Die): An instance of Card.
+        """
+        self.value = 0
+
+    def guess_card(self):
+        """Selects a new random card.
+        
+        Args:
+            self (Card): An instance of Hilo.
+        """
+        self.value = random.randint(1, 13)
